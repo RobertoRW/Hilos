b5a92e4a64dd6d3bc3f782efdc54be998de0684d
Roberto Ruiz <robertorrw@gmail.com>
2014-04-09 03:55:15 -0500

    comentarios en el codigo de la lista

--- lista.c
+++ lista.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "control.h"
-
+/* lista.c
+/  Mantiene la lista enlazada */
 void recorrer(void);
 
-static nodo *primero = NULL; 
+static nodo *primero = NULL; /* primer nodo de la lista */ 
 
+/* la funcion main es solo para las pruebas */
 void main() {
     agregar("A", 3, 2);
     agregar("E", 1, 2);
@@ -30,7 +32,7 @@ void main() {
         printf("Nodo %s inicia en %d\n", lol->ID, lol->inicio);
 
 }
-
+/* agrega un nodo con el nombre, inicio y longitud dados */
 void agregar(char *nombre, int ini, int lon)
 {
     nodo *cur, *nuevo_nodo, *prev;
@@ -48,31 +50,37 @@ void agregar(char *nombre, int ini, int lon)
     nuevo_nodo->compresor_esperando = 0;
     nuevo_nodo->destructor_esperando = 0;
 
+    /* Busca la posicion que le corresponde para mantener la lista ordenada */
     for (cur = primero, prev = NULL; cur != NULL && cur->inicio <= ini;
             prev = cur, cur = cur->siguiente)
     ;
 
     nuevo_nodo->siguiente = cur;
-    if (prev == NULL) {
+    if (prev == NULL) {     /* si la lista esta vacia */
         primero = nuevo_nodo;
     } else
         prev->siguiente = nuevo_nodo;
 }
 
+/* obtiene el nodo con el nombre dado */
 nodo *obtener(char *nombre)
 {
     nodo *cur;
 
+    /* recorre la lista hasta que encuentre el elemento o hasta que se termine.
+       strcmp(s,s) compara que dos strings sean iguales, retorna 0 si son
+       iguales */
     for (cur = primero; cur != NULL && strcmp(cur->ID, nombre);
             cur = cur->siguiente)
         ;
 
     if (cur == NULL)
-        return NULL;
+        return NULL;    /* no se encontro nada */
     else if(strcmp(cur->ID, nombre) == 0)
         return cur;
 }
 
+/* elimina de la lista el nodo con el nombre dado */
 void eliminar(char *nombre)
 {
     nodo *cur, *prev;
@@ -82,9 +90,9 @@ void eliminar(char *nombre)
         ; 
     
     if(cur == NULL)
-        return;
+        return;      /* si no lo encontro */
 
-    if(prev == NULL)
+    if(prev == NULL) /* si es el primer elemento */
         primero = cur->siguiente;
     else if(strcmp(cur->ID, nombre) == 0)
         prev->siguiente = cur->siguiente;
@@ -93,6 +101,8 @@ void eliminar(char *nombre)
     return;
 }
 
+/* esta funcion no sirve de nada para la tarea, la uso para pruebas
+   recorre la liste e impreme el nombre con el inicio de cada elemento */
 void recorrer(void)
 {
     nodo *cur;
