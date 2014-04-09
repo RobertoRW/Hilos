bc1c10e6bf3c1e3620e747ceb6bdf5e2b3819e55
Roberto Ruiz <robertorrw@gmail.com>
2014-04-08 22:47:37 -0500

    Estructura de nodo tentativa

--- control.h
+++ control.h
@@ -1,16 +1,16 @@
 struct nodo_control_struct {
-	char[2] ID;
-	int inicio;
-	int longitud;
-	char[3] ultimo_escritor;
-	int escritor_adentro;
-	int lector_adentro;
-	int destructor_adentro;
+    char[2] ID;
+    int inicio;
+    int longitud;
+    char[3] ultimo_escritor;
+    int escritor_adentro;
+    int lector_adentro;
+    int destructor_adentro;
     int escritores_esperando;
     int lectores_esperando;
     int compresor_esperando;
     int destructor_esperando;
-	struct nodo_control_struct *siguiente;
+    struct nodo_control_struct *siguiente;
 };
 
 typedef struct nodo_control_struct nodo;
